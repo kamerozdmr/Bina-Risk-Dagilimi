0922384e619ca894700f9cdaeb2577dfa9efdacd
Kamer Özdemir <kamer.oz@outlook.com>
2022-11-18 02:29:34 +0300

    Add files via upload

---
+++ pages/11_Diğer_Bilgiler.py
@@ -0,0 +1,48 @@
+import streamlit as st
+
+# Streamlit ana menüsünü ve footerı sayfadan kaldır
+st.markdown(""" <style> #MainMenu {visibility: hidden;} footer {visibility: hidden;} </style> """, unsafe_allow_html=True)
+
+
+# auth kontrol et 
+if "authentication_status" not in st.session_state:
+    st.session_state["authentication_status"] = None
+
+if st.session_state["authentication_status"] == False:
+    st.warning("Devam etmek için Ana Sayfaya dönüp giriş yapınız.")
+    # Yan bar bilgi mesajı
+    st.sidebar.warning("Giriş yapılmadı")
+
+if st.session_state["authentication_status"] == None:
+    st.warning("Devam etmek için Ana Sayfaya dönüp giriş yapınız.")
+    # Yan bar bilgi mesajı
+    st.sidebar.warning("Giriş yapılmadı")
+
+
+
+if st.session_state["authentication_status"]:
+    # İlerlemeyi gösteren bar ekle
+    prg_percent = 90 # İlerleme yüzdesi
+    progress_bar = st.progress(prg_percent)
+
+    # Yan bar bilgi mesajı
+    st.sidebar.success("Yukarıdaki değerlendirme yöntemlerini sıra ile doldurunuz.")
+
+    st.title("Diğer Bilgiler")
+    st.write("Verilen parametreler dışında bina hakkında girmek istediğiniz bilgileri aşağıdaki alana yazabilrisiniz.")
+
+
+
+    if "diger_bilgiler" not in st.session_state:
+        st.session_state["diger_bilgiler"] = ""
+
+
+    diger_bilgiler = str(st.text_area(st.session_state["diger_bilgiler"], height=200))
+    coord_submit = st.button("Kaydet")
+
+    if coord_submit:
+        st.session_state["diger_bilgiler"] = diger_bilgiler
+        st.success("Kaydedildi.")
+
+    # İlerleme barını güncelle
+    progress_bar.progress(prg_percent+10)
